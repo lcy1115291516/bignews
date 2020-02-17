8b93f2d3fcf0439857893e32a42f67b42c0ca968
liu <tezuka90@126.com>
2020-02-17 21:11:26 +0800

    后台文章新增

---
+++ static/admin/js/article-release.js
@@ -0,0 +1,64 @@
+// alert(1)
+
+
+// 图片预览
+$("#exampleInputFile").on('change', function () {
+    // alert(1)
+    var file = this.files[0]
+    var imgURL = URL.createObjectURL(file);
+    $("#preview").prop('src', imgURL)
+});
+
+
+// 收集表单信息，给form表单注册submit事件
+// $("#publishForm").on('submit', function () {
+//     // alert(1)
+//     // 收集表单信息  给表单添加name属性
+//     var formdata = new FormData(this);
+//     var release = $(this).find('#release');
+//     if (release.val() == '发布') {
+//         formdata.append('state', 1)
+//     }
+//     console.log(formdata)
+//     // 发送请求
+//     $.ajax({
+//         type: 'post',
+//         url: 'http://localhost:8080/api/v1/admin/article/publish',
+//         data: formdata,
+//         processData: false,
+//         contentType: false,
+//         success: function (result) {
+//             // console.log(result)
+//             // 刷新页面
+//             location.reload();
+//         }
+//     })
+//     // 阻止表单的默认行为
+//     return false;
+// });
+
+$("#release").on('click', function () {
+    var formdata = new FormData($("#publishForm")[0]);
+    formdata.append('state', '已发布');
+    getdata(formdata);
+});
+
+$("#draft").on('click', function () {
+    var formdata = new FormData($("#publishForm")[0]);
+    getdata(formdata);
+});
+
+function getdata(data) {
+    $.ajax({
+        type: 'post',
+        url: 'http://localhost:8080/api/v1/admin/article/publish',
+        data: data,
+        processData: false,
+        contentType: false,
+        success: function (result) {
+            // console.log(result)
+            // 刷新页面
+            location.reload();
+        }
+    })
+}
