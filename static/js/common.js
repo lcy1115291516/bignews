a9456ce5a37e0413a01d1c63b499143d5949388f
wangxiaotian <1559925054@qq.com>
2020-02-18 13:29:12 +0800

    热门排行模块

--- static/js/common.js
+++ static/js/common.js
@@ -40,3 +40,22 @@ $.ajax({
         console.log(html);
     }
 })
+
+// 向服务器端发送请求，索要热点排行数据
+$.ajax({
+    type: 'get',
+    url: 'http://localhost:8080/api/v1/index/rank',
+    success: function (response) {
+        console.log(response);
+        var commentTpl = `
+        {{each data}}
+        <li><span>{{$index+1}}</span><a href="">{{$value.title}}</a></li>
+          {{/each}}
+        `;
+        var html = template.render(commentTpl, {
+            data: response.data
+        });
+        $('#commentpaihang').html(html);
+        console.log(html);
+    }
+})
