e05e94b18a0de59e6112e94d6ce2cf3c1e63a8a2
liu <tezuka90@126.com>
2020-02-19 11:35:02 +0800

    前台搜索功能

--- static/js/search.js
+++ static/js/search.js
@@ -14,6 +14,40 @@ $.ajax({
             data: response.data.data
         });
         $('#lasteBox').html(html);
+        $("#searchend").html('<h3>搜索结果</h3>');
+        $("#pagination").pagination({
+            currentPage: response.data.page,
+            totalPage: response.data.pages,
+            callback: function (current) {
+                changePage(current)
+            }
+        });
 
     }
 });
+
+function changePage(p) {
+    $.ajax({
+        url: 'http://localhost:8080/api/v1/index/search',
+        data: {
+            key: decodeURI(key),
+            page: p
+        },
+        success: function (response) {
+            console.log(response);
+
+            var html = template('searchTpl', {
+                data: response.data.data
+            });
+            $('#lasteBox').html(html);
+            $("#searchend").html('<h3>搜索结果</h3>');
+            $("#pagination").pagination({
+                currentPage: response.data.page,
+                totalPage: response.data.pages,
+                callback: function (current) {
+                    changePage(current)
+                }
+            });
+        }
+    })
+}
